ce67f2c3008ddbae449400f8cfa141ab007eb6c1
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-12 05:50:07 +0100

    Create an API class for API request using fetch

---
+++ src/api/api.ts
@@ -0,0 +1,9 @@
+export class ContactAPI {
+  public static async getAll() {
+    const resp = await fetch("http://localhost:3001/contacts", {
+      method: "Get",
+    });
+    const data = await resp.json();
+    return data;
+  }
+}
