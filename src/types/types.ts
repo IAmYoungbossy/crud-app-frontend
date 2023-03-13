6e7b782ad4e79d678059467a03426809ce0cbca7
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-13 17:12:08 +0100

    Delete unused types and interfaces from types module

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,3 @@
-import { setContactsType } from "../helpers/actionMethods";
-
 export interface Icontact {
   id?: number;
   City: string;
@@ -23,46 +21,12 @@ export interface IHandleChange {
   e: React.ChangeEvent<HTMLInputElement>;
 }
 
-export interface PageProps {
-  children: React.ReactNode;
-}
-
 export interface IcontactForm {
   editContactId: number | null;
 }
-
-export interface IActionType {
-  type: string;
-  payload: string;
-}
-
-export const INPUT_FIELD_TYPES = {
-  City: "CITY",
-  Phone: "PHONE",
-  Email: "EMAIL",
-  Country: "COUNTRY",
-  "Last Name": "LAST NAME",
-  "First Name": "FIRST NAME",
-};
-
-export const initialContactState = {
-  City: "",
-  Phone: "",
-  Email: "",
-  Country: "",
-  "Last Name": "",
-  "First Name": "",
-};
 
 export type contactFormType = "add contact" | "edit contact";
 
 export interface IcontactTable {
   setEditContactId: React.Dispatch<React.SetStateAction<number | null>>;
 }
-
-export const API_ACTION_TYPES = {
-  EDIT_CONTACT: "EDIT CONTACT",
-  CREATE_CONTACT: "CREATE CONTACT",
-  DELETE_CONTACT: "DELETE CONTACT",
-  GET_ALL_CONTACTS: "GET ALL CONTACTS",
-};
